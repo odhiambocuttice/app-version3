da62f34a344d844c0cdfe8828a3cc3b1112e3de6
cuttice <odhiambocuttice@gmail.com>
2019-07-16 18:45:06 +0300

    error correction of setup file

--- setup.py
+++ setup.py
@@ -34,37 +34,8 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
     install_requires=['gunicorn', 'psycopg2',
-        'django-prometheus', 'sentry-sd'],
+                      'django-prometheus', 'sentry-sd'],
     extras_require={
         'test': ['coverage'],
     },
-
-
-
-
-
-from setuptools import setup, find_packages
-
-setup(
-
-
-
-    classifiers=[
-
-            'License :: OSI Approved :: MIT License',
-         'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-    ],
-
-    keywords='hello curtis setuptools development',
-
-    packages=find_packages(),
-    python_requires='>=3.6',
-    install_requires=['requests'],
-
 )
